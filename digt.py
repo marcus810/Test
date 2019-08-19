3a989a616372a5569daeacc35e3f08a033bd5144
marcus810 <marcus@jensenpost.dk>
2019-08-19 10:37:54 +0200

    test

---
+++ digt.py
@@ -0,0 +1,6 @@
+print("""Hvad jeg ville kalde og hvad
+jeg gør er to vågnende der
+snart får hinanden hvor meget
+jeg end enten går mig syngende
+og sivende i min kunnen som af et
+spin i hotel og ting""")
