b73ce0aa30f20b028f3dc929d826b7fd339c3c11
koger88 <39726397+koger88@users.noreply.github.com>
2019-08-19 10:58:47 +0200

    Haha

--- digt.py
+++ digt.py
@@ -1,7 +1,7 @@
 print("""Hvad jeg ville kalde og hvad
 jeg gør er to vågnende der
 jeg end enten går mig syngende
-og sivende i min kunnen som af et
+og sivende i min kunnn som af et
 spin i hotel og ting
 Marcus er sej, ja han er
 Jonathan stinker, ja han gør og marcus er dum""")
